e3a881ce78c207461f09202e711413ec18c465d7
FenZlo <daniel.uporov@gmail.com>
2024-01-06 17:33:17 +0500

    Add new function in data_base

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ from loguru import logger
 from datetime import date
 from calendar import monthrange
 
+from data_base import create_db_and_base, is_course_on_date_exist
 
-def get_datime_range(year: int, month: int) -> list[date]:
+YEAR = 2023
+MONTH = 11
+
+
+def get_datime_range(year=YEAR, month=MONTH) -> list[date]:
     last_day = monthrange(year, month)[1]
-    return [date(year, month, day) for day in range(1, last_day+1)]
+    return [date(YEAR, MONTH, day) for day in range(1, last_day + 1)]
 
 
-def get_curse(date_: date, base_currency: str = 'eur', target_currency: str = 'rub') -> float:              #сигнатура функции
+def get_curse(date_: date, base_currency: str = 'eur', target_currency: str = 'rub') -> float:  # сигнатура функции
     url = f'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/{date_.strftime("%Y-%m-%d")}/currencies/{base_currency}.json'
     response = request(method='get', url=url)
     json = response.json()
@@ -19,8 +24,7 @@ def get_curse(date_: date, base_currency: str = 'eur', target_currency: str = 'r
 
 
 def get_currency_curse_on_date(base_currency='eur', target_currency='rub'):
-    year, month = 2023, 11
-    range_date = get_datime_range(year, month)
+    range_date = get_datime_range()
 
     for date_ in range_date:
 
@@ -28,11 +32,14 @@ def get_currency_curse_on_date(base_currency='eur', target_currency='rub'):
             curse = get_curse(date_, base_currency, target_currency)
 
         except JSONDecodeError as e:
-            logger.error(f'Error retrieving data for {date_}: {e}')
+            logger.warning(f'Error retrieving data for {date_}: {e}')
             continue
 
         logger.debug(f'Date: {date_} curse: {curse}')
 
+        is_course_on_date_exist(date_)
+
 
 if __name__ == '__main__':
+    create_db_and_base()
     get_currency_curse_on_date()
